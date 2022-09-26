970cf80e3accefcf33104971dff11b2d81270966
Yash Dedania <yashdedania2002@gmail.com>
2022-09-26 11:49:10 +0530

    Implemented the code for capturing the image with date and time using datetime library

--- StudentAttendance.py
+++ StudentAttendance.py
@@ -1,8 +1,18 @@
-import boto3
 import requests
 import datetime
 import time
 import cv2
 
 
+for j in range(0, 1):
+    current_time = datetime.datetime.now().strftime("%d-%m-%y  %H-%M-%S ")
+    
+    print(current_time)
+    camera = cv2.VideoCapture(0)
+    for i in range(20):
+        return_value, image = camera.read()
+        if (i == 19):
+            cv2.imwrite('G:/Vs code/AI-Powered-Hourly-Attendance-Capturing-System-master/' + current_time + '.jpg', image)
+    del (camera)
 
+    time.sleep(20)
